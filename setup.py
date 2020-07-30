79611e14fb0f11ca66c0593b7f6aac24507b5dfb
Giuliano-C <celanigiuliano@gmail.com>
2020-07-30 14:29:56 +1000

    bump version 0.02

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 import os
 from setuptools import setup, find_packages
 
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(
+    os.path.join(here, "uni_scraper", "__version__.py"), "r", encoding="utf-8"
+) as f:
+    exec(f.read(), about)
+
 README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -8,7 +15,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name="uni_scrapers",
     url="https://github.com/Giuliano-C/UniScraper/",
-    version="0.0.1",
+    version=about['__version__'],
     author="Giuliano Celani",
     description="Python package for scraping university information",
     keywords="python uni university scraper uni-scraper",
